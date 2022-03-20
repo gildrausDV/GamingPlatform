40090c28ed54f100672ef597dc97da8ce5397ea3
Dimitrije Vujcic <vd190341d@student.etf.bg.ac.rs>
2022-03-20 20:09:20 +0100

    Prototip - tournament.html

---
+++ Prototip/tournament.js
@@ -0,0 +1,32 @@
+let started = false;
+let ended = false;
+
+var timerInterval;
+
+function join() {
+    if(ended = true) {
+        ended = false;
+        started = false;
+    }
+    if(started == true) {
+        return;
+    }
+    started = true;
+    document.getElementById("notification").innerText = "You have successfully joined the tournament!";
+    timerInterval = setInterval(function start_end() {
+        //join();
+        ended = true;
+        document.getElementById("notification").innerText = "";
+        clearInterval(timerInterval);
+    }, 1500);
+}
+
+function back() {
+    location.href = "index.html";
+}
+function signOut_page() {
+    location.href = "index.html";
+}
+function addTournament() {
+    location.href = "addTournament.html";
+}
