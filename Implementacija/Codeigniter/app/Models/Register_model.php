08a675ff8db7f246eacf441f447017f2e3c3a8c8
Bogdan Jovanovic <jb190335d@student.etf.bg.ac.rs>
2022-05-31 18:50:33 +0200

    Dodato koriscenje ajax-a u registraciji

--- Implementacija/Codeigniter/app/Models/Register_model.php
+++ Implementacija/Codeigniter/app/Models/Register_model.php
@@ -10,30 +10,30 @@ class Register_model extends Model {
     
     public function register() {
 
-        $forename = $_POST['forename'];
-        $surname = $_POST['surname'];
-        $email = $_POST['email'];
-        $user = $_POST['username'];
-        $pass = $_POST['password'];
-        $date = $_POST['date'];
-        $confirmPass = $_POST['confirmPassword'];
+        $forename = $_POST['f'];
+        $surname = $_POST['s'];
+        $email = $_POST['e'];
+        $user = $_POST['u'];
+        $pass = $_POST['p'];
+        $date = $_POST['d'];
+        /*$confirmPass = $_POST['confirmPassword'];
         if ($forename == "" || $surname == "" || $email == "" || $user == "" || $pass == "" ||
         $confirmPass == "" || $date == "") {
             return 1;
-        }
+        }*/
 
         $exists = $this->table('user')->select()->where('username', $user)->paginate(1);
         if(count($exists) == 1) {
-            return 2;
+            return 0;
         }
 
-        if (strlen($pass) < 5) {
+        /*if (strlen($pass) < 5) {
             return 3;
         }    
 
         if ($pass != $confirmPass) {
             return 4;
-        }
+        }*/
 
         $data = [
             'username' => $user,
@@ -53,7 +53,8 @@ class Register_model extends Model {
         $this->insert($data);
         //$this->table('user')->insert($data);
 
-        return 5;
+        //return 5;
+        return 1;
     }
 
 }
--- Implementacija/Codeigniter/public/assets/scripts/register.js
+++ Implementacija/Codeigniter/public/assets/scripts/register.js
@@ -25,4 +25,45 @@ $(document).ready(function() {
         }
         hidden2 = !hidden2;
     });
+
+    $("#submit").click(function() {
+        let forename = $("#forename").val();
+        let surname = $("#surname").val();
+        let email = $("#email").val();
+        let date = $("#date").val();
+        let username = $("#username").val();
+        let password = $("#password").val();
+        let confirm = $("#confirmPassword").val();
+        if (forename == "" || surname == "" || email == "" || date == "" || username == "" || password == "" || confirm == "") {
+            $("#notification").css("color", "red").text("Please provide necessary information!");
+            return;
+        }
+        
+        if (password.length < 5) {
+            $("#notification").css("color", "red").text("Password too short!");
+            return;
+        }
+
+        if (password != confirm) {
+            $("#notification").css("color", "red").text("Passwords do not match!");
+            return;
+        }
+
+        $.ajax({
+            method: "POST",
+            url: window.location.origin + "/Home/register_",
+            data: {f: forename, s: surname, e: email, d: date, u: username, p: password},
+            success: function (obj, textstatus) {
+                if (obj == 0) {
+                    $("#notification").css("color", "red").text("Username already exists!");
+                }
+                else if (obj == 1) {
+                    $("#notification").css("color", "green").text("Registration successful!");
+                }
+            },
+            error: function(xhr, status, error) {
+                alert(xhr.responseText + " " + error + " " + status);
+            }
+          });
+    });
 });
