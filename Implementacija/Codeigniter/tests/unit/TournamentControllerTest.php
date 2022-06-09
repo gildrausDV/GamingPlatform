2a401103ad1483db02da907ad419c634c864f254
Bogdan Jovanovic <jb190335d@student.etf.bg.ac.rs>
2022-06-10 01:09:56 +0200

    Odradjeni neki testovi za Tournament kontroler

--- Implementacija/Codeigniter/tests/unit/TournamentControllerTest.php
+++ Implementacija/Codeigniter/tests/unit/TournamentControllerTest.php
@@ -12,26 +12,42 @@ final class TournamentControllerTest extends CIUnitTestCase {
     use ControllerTestTrait;
     use DatabaseTestTrait;
     
-    /*public function testIndex() {
+    public function testIndex() {
+        
+        $_SESSION['role'] = 0;
         $result = $this//->withURI('http://localhost:8080/...')
             ->controller(\App\Controllers\Tournament::class)
             ->execute('index');
     
         $this->assertTrue($result->isOK());
-    }*/   
 
-    /*public function provide_ids_roles() {
+        $_SESSION['role'] = 1;
+        $result = $this//->withURI('http://localhost:8080/...')
+            ->controller(\App\Controllers\Tournament::class)
+            ->execute('index');
+    
+        $this->assertTrue($result->isOK());
+
+        $_SESSION['role'] = 2;
+        $result = $this//->withURI('http://localhost:8080/...')
+            ->controller(\App\Controllers\Tournament::class)
+            ->execute('index');
+    
+        $this->assertTrue($result->isOK());
+    }
+
+    public function provide_ids_roles() {
         return [
             [1, 2],
             [2, 1],
             [3, 0]
         ];
-    }*/
+    }
 
     /**
      * @dataProvider provide_ids_roles
      */
-    /*public function testTournament($id, $role) {
+    public function testTournament($id, $role) {
         $_SESSION['ID'] = $id;
         $_SESSION['role'] = $role;
         $result = $this//->withURI('http://localhost:8080/...')
@@ -39,17 +55,25 @@ final class TournamentControllerTest extends CIUnitTestCase {
             ->execute('tournament');
     
         $this->assertTrue($result->isOK());
-    }*/
+    }
 
-    /*public function testAddTournament() {
+    public function testAddTournament() {
+        $_SESSION['role'] = 1;
         $result = $this//->withURI('http://localhost:8080/...')
             ->controller(\App\Controllers\Tournament::class)
             ->execute('addTournament');
     
         $this->assertTrue($result->isOK());
-    }*/
 
-    /*public function provide_ids_roles_tournaments() {
+        $_SESSION['role'] = 2;
+        $result = $this//->withURI('http://localhost:8080/...')
+            ->controller(\App\Controllers\Tournament::class)
+            ->execute('addTournament');
+    
+        $this->assertTrue($result->isOK());
+    }
+
+    public function provide_ids_roles_tournaments() {
         return [
             [1, 2, 1],
             [1, 2, 2],
@@ -61,12 +85,12 @@ final class TournamentControllerTest extends CIUnitTestCase {
             [3, 0, 2],
             [3, 0, 3]
         ];
-    }*/
+    }
 
     /**
-     * dataProvider provide_ids_roles_tournaments
+     * @dataProvider provide_ids_roles_tournaments
      */
-    /*public function testPlayerList($id, $role, $tournament) {
+    public function testPlayerList($id, $role, $tournament) {
         $_SESSION['ID'] = $id;
         $_SESSION['role'] = $role;
         $result = $this//->withURI('http://localhost:8080/...')
@@ -74,15 +98,15 @@ final class TournamentControllerTest extends CIUnitTestCase {
             ->execute('playerList', $tournament);
     
         $this->assertTrue($result->isOK());
-    }*/
+    }
 
-    /*public function testEndTournament() {
+    public function testEndTournament() {
         $result = $this//->withURI('http://localhost:8080/...')
             ->controller(\App\Controllers\Tournament::class)
             ->execute('endTournament');
     
         $this->assertTrue($result->isOK());
-    }*/
+    }
 
     /*public function testIsActive() {
         if(!isset($_SESSION['ID'])) $_SESSION['ID'] = 1;
